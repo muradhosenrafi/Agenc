8224ad19fc305f2e397d0221a697fa34c7cdc735
muradhosenrafi <muradhosenrafi@gmail.com>
2026-05-25 01:39:24 +0600

    update

--- src/components/AgenceCard.jsx
+++ src/components/AgenceCard.jsx
@@ -7,7 +7,7 @@ const AgenceCard = ({agencimg,text,title,className}) => {
   return (
     <div className={`w-[344px] h-[176px] flex flex-col  justify-start ${className}`}>
      <Image src={agencimg}/>
-     <Headding className=" pt-5 pb-2.5 text-[25px]" text={text}/>
+     <Headding className=" pt-5 pb-2.5 !text-[25px]" text={text}/>
      <Para className="!w-[330px]" text={title}/>
     </div>
   )
